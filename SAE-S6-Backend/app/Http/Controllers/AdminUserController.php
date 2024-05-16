4e142eb847ec21eb819bc3a80a27b8d74f8215e4
FrancoisToureille <francois.toureille@etu.univ-amu.fr>
2024-05-16 09:10:08 +0200

    ajout export

--- SAE-S6-Backend/app/Http/Controllers/AdminUserController.php
+++ SAE-S6-Backend/app/Http/Controllers/AdminUserController.php
@@ -7,6 +7,8 @@ use App\Models\AdminUser;
 use App\Models\Response;
 use Illuminate\Http\Request;
 use ReturnTypeWillChange;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+
 
 class AdminUserController extends Controller
 {
@@ -55,9 +57,43 @@ class AdminUserController extends Controller
 
         return response()->json(null, 204);
     }
-    
+
 
     public function exportData(Request $request) {
-       return response()->json(Response::all());
-    }
+            // Récupérer toutes les instances de Response avec les relations 'question' et 'choice'
+            $responses = Response::with(['question', 'choice'])->get();
+    
+            // Définir le nom du fichier CSV
+            $filename = 'responses.csv';
+    
+            // Créer une réponse en flux (streamed response)
+            $response = new StreamedResponse(function() use ($responses) {
+                // Ouvrir le flux en écriture
+                $handle = fopen('php://output', 'w');
+    
+                // Ajouter l'en-tête du fichier CSV
+                fputcsv($handle, ['Response ID', 'Question Libelle', 'Question Description',  'Choice Value', 'Response Content', 'Created At'], ";");
+    
+                // Ajouter les données des réponses
+                foreach ($responses as $response) {
+                    fputcsv($handle, [
+                        $response->id,
+                        $response->question->title ?? '', // Accès au libelle de la question
+                        $response->question->description ?? '', // Accès au libelle de la question
+                        $response->choice->text ?? '', // Accès à la valeur du choix
+                        $response->response_text,
+                        $response->created_at,
+                    ], ";");
+                }
+    
+                // Fermer le flux
+                fclose($handle);
+            });
+    
+            // Définir les en-têtes HTTP pour forcer le téléchargement
+            $response->headers->set('Content-Type', 'text/csv');
+            $response->headers->set('Content-Disposition', 'attachment; filename="' . $filename . '"');
+    
+            return $response;
+        }
 }
