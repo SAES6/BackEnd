0d815fcd883fcfe9a712b9e18a55b8323d7fd243
Lucas JOLY <lucas.joly.0903@gmail.com>
2024-06-03 09:01:53 +0200

    firebase

--- SAE-S6-Backend/app/Http/Controllers/QuestionController.php
+++ SAE-S6-Backend/app/Http/Controllers/QuestionController.php
@@ -32,11 +32,24 @@ class QuestionController extends Controller
         $sliderMin = $request->sliderMin ?? null;
         $sliderMax = $request->sliderMax ?? null;
         $sliderGap = $request->sliderGap ?? null;
+        $newType = $request->type;
+        if($request->type == 'Curseur'){
+            $newType = 'slider';
+        }
+        else if($request->type == 'Choix multiple'){
+            $newType = 'multiple_choice';
+        }
+        else if($request->type == 'Choix unique'){
+            $newType = 'single_choice';
+        }
+        else if($request->type == 'Libre'){
+            $newType = 'text';
+        }
         $question = Question::create([
             'question' => $request->question,
             'section_id' => $request->section_id,
             'questionnaire_id' => $request->questionnaire_id,
-            'type' => $request->type,
+            'type' => $newType,
             'img_src' => $imageUrl,
             'title' => $request->title,
             'description' => $request->description,
@@ -63,6 +76,18 @@ class QuestionController extends Controller
             $questions = Question::where('section_id', $section_id)->with('choices')->get();
             foreach ($questions as $question) {
                 if($question->img_src != null) $question->img_src = (new ImageController)->generateSignedUrl($question->img_src);
+                if($question->type == 'slider'){
+                    $question->type = 'Curseur';
+                }
+                else if($question->type == 'multiple_choice'){
+                    $question->type = 'Choix multiple';
+                }
+                else if($question->type == 'single_choice'){
+                    $question->type = 'Choix unique';
+                }
+                else if($question->type == 'text'){
+                    $question->type = 'Libre';
+                }
             }
             return response()->json($questions, 200);
         }
